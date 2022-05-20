55f74a54df1954cc245cf70115694852343e5c32
Kennedy Mota <kennedysantosmota22@gmail.com>
2022-05-20 18:20:09 -0300

    exercise 1

---
+++ lógica de programação/exercise 1/exercise 1 - user data.js
@@ -0,0 +1,8 @@
+alert("Bem-vindo! A seguir pediremos que informe alguns dados")
+let userName = prompt("Informe o seu usuário")
+let userAge = prompt("Informe a sua idade")
+let userAgeConfirm = confirm("A sua idade é " + userAge + " anos?")
+
+alert("Nome " + userName)
+alert("Idade " + userAge)
+alert("Idade confirmada " + userAgeConfirm)
