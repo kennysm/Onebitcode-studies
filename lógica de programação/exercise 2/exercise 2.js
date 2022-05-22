be58e7694d40e52b46815cda1b8ff9083f0ec84f
Kennedy Mota <kennedysantosmota22@gmail.com>
2022-05-21 23:11:00 -0300

    exercise 2

---
+++ lógica de programação/exercise 2/exercise 2.js
@@ -0,0 +1,8 @@
+let OlderPersonName = prompt ("Qual o nome da pessoa mais velha ?")
+let OlderPersonAge = prompt ("Qual a idade da pessoa mais velha ?")
+let YoungerPersonName = prompt ("Qual o nome da pessoa mais nova ?")
+let YoungerPersonAge = prompt ("Qual a idade da pessoa mais nova ?")
+let AgeDifference = OlderPersonAge - YoungerPersonAge
+
+alert("Pessoa mais Velha: " + OlderPersonName + "\nIdade: " + OlderPersonAge + "\nPessoa mais nova: " 
++ YoungerPersonName + "\nIdade: " + YoungerPersonAge + "\nDiferença de idade: " + AgeDifference)
