7d0c29512ff921c769c37a966bb498b4d1cb60ad
Kennedy Mota <kennedysantosmota22@gmail.com>
2022-05-22 23:44:54 -0300

    exercise 4 - conditionals

---
+++ lógica de programação/Condicionais/exercise 4.js
@@ -0,0 +1,9 @@
+let velocity = 101
+
+if(velocity <40) {
+  console.log("Velocidade baixa")
+} else if(velocity <= 100) {
+  console.log("Cuidado! Próximo a velocidade de risco")
+} else {
+    console.log("velocidade de Risco!")
+  }
