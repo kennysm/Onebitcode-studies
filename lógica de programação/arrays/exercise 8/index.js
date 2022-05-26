b438d22bb80a72762487f6f9c2f6fd707ca86a60
Kennedy Mota <kennedysantosmota22@gmail.com>
2022-05-25 21:38:47 -0300

    exercise 8 arrays

---
+++ lógica de programação/arrays/exercise 8/index.js
@@ -0,0 +1,25 @@
+const hitchedSpaceship = [
+  ["Fenix", 8, true],
+  ["Golias", 10, true],
+  ["Helmet", 5, false],
+  ["Elemental", 3, true],
+  ["Darwin", 15, false],
+]
+
+let crewGreatherThan9 = hitchedSpaceship.filter(spaceship => {
+  return spaceship[1] > 9
+}).map(spaceship => {
+  return spaceship[0]
+})
+
+let ongoingHitchPlataform = hitchedSpaceship.findIndex(spaceship => {
+  return spaceship[2] == false
+})
+
+let highLightedSpaceship = hitchedSpaceship.map(spaceship => {
+  return highLightedSpaceship[0].toUpperCase()
+})
+
+let message = "Espaçonaves com mais de 9 tripulantes: " + crewGreatherThan9.join(", ")
+message += "\nPlataform com processo de engate: " + (ongoingHitchPlataform + 1)
+message += "\nEspaçonaves destacadas: " + hitchedSpaceship.join(", ")
