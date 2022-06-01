da69aac6826e7dbefc220372c3480861afcf691f
Kennedy Mota <kennedysantosmota22@gmail.com>
2022-05-31 22:19:06 -0300

    exercise 12 DOM events

---
+++ Manipulando a DOM/Events/exercise 12/events.js
@@ -0,0 +1,19 @@
+function clickMe() {
+  console.log("O botão foi clicado")
+}
+
+function focusMe() {
+  console.log("Fui focado!")
+}
+
+function blurMe() {
+  console.log("Fui desfocado!")
+}
+
+function changeMe() {
+  console.log("Fui alterado!")
+}
+
+function overMe() {
+  console.log("sobre mim")
+}
