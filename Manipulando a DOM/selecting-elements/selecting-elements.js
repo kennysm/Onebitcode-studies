bb36be171af4f901cc1d2353e708709f7446128e
Kennedy Mota <kennedysantosmota22@gmail.com>
2022-06-02 21:29:56 -0300

    selecting-elements examples

---
+++ Manipulando a DOM/selecting-elements/selecting-elements.js
@@ -0,0 +1,14 @@
+function show() {
+  let element = document.getElementsByTagName("input")
+  console.log(element)
+}
+
+function show() {
+  let element = document.getElementById("names")
+  console.log(element)
+}
+
+function show() {
+  let element = document.querySelectorAll("div#phones input [name='phone']")
+  console.log(element)
+}
