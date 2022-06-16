2cb56ba5ce731cc3d532786355e4d1a5ac3c5448
Kennedy Mota <kennedysantosmota22@gmail.com>
2022-06-15 22:28:08 -0300

    desvendando o webpack2

---
+++ Desvendando o Webpack/webpack.config.js
@@ -0,0 +1,12 @@
+const path = require("path")
+module.exports = {
+  entry: {
+    index: './src/index.js',
+    galaxy: './src/galaxy.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+}
+
