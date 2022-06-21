ffc9092e615aee3abf59415b5e80517561ad0a1d
Kennedy Mota <kennedysantosmota22@gmail.com>
2022-06-20 22:09:15 -0300

    CommonJS

--- Introdução_modulo/src/index.js
+++ Introdução_modulo/src/index.js
@@ -1,1 +1,12 @@
-console.log("teste")
+const Planet = require('./planet')
+const solarSystem = require('./solar_system')
+
+const earth = new Planet("Terra", 50100000)
+earth.rotate()
+
+solarSystem.planets.push(earth)
+solarSystem.planets.push(new Planet("Marte", 144800000))
+solarSystem.planets.push(new Planet("Mercúrio", 74800000))
+solarSystem.planets.push(new Planet("Saturno", 42700000000))
+
+console.log(solarSystem)
---
+++ Introdução_modulo/src/planet.js
@@ -0,0 +1,11 @@
+class Planet {
+  constructor(name, surfaceArea) {
+    this.name = name
+    this.surfaceArea = surfaceArea
+  }
+  rotate() {
+    console.log("Rotacionando!")
+  }
+}
+
+module.exports = Planet
