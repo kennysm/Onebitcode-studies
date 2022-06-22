28a73471cfa9ed2c87e4fb6c7d8f7e8bd83a2107
Kennedy Mota <kennedysantosmota22@gmail.com>
2022-06-21 22:42:35 -0300

    promises

--- Introdução_modulo/src/index.js
+++ Introdução_modulo/src/index.js
@@ -1,13 +1,22 @@
-function step02() {
-  console.log("passo 02")
+let komodoship = {
+  name: "Komodo",
+  velocity: 80,
+  acceleration: 10
 }
+const velocityAfter2Seconds = (velocity, acceleration) => {
+  new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      if (acceleration > 0) {
+        velocity += acceleration * 2
+        console.log(`Nova velocidade: ${velocity}`)
+        resolve(velocity)
+      } else {
+        console.log("Aceleração inválida")
+        reject("Não possui aceleração")
+      }
+    }, 1000)
+  })
+}
+velocityAfter2Seconds(komodoship.velocity, komodoship.acceleration)
 
-console.log("Passo 01")
-step02()
-console.log("passo 03")
-
-console.log("Passo 04")
-setTimeout(() => {
-  console.log("Passo 05")
-}, 1000)
-console.log("Passo 06")
+console.log("Execução ed Promises")
